07b633239cf6840ae8f93e1d94982e5d15e2289e
sam marshall <s.marshall@open.ac.uk>
2013-01-22 11:39:35 +0000

    OU search: Remove old 'displayversion' code #5698

--- version.php
+++ version.php
@@ -1,4 +1,26 @@
 <?php
-$plugin->version = 2013011800;
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version.
+ *
+ * @package local_ousearch
+ * @copyright 2013 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$plugin->version = 2013012200;
 $plugin->requires = 2011120100;
 $plugin->displayversion = 'Unstable development version (use at own risk)';
